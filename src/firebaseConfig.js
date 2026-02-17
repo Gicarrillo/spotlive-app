b1c822e3b8292a153789b84a0080edf021edadc2
Raqueldeleon62 <2024310112@uteq.edu.mx>
2026-02-17 11:51:17 -0600

    Inicializar proyecto React en repositorio

---
+++ src/firebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDeOC84QxXYUf1_86XwgGgKPcAEWUzkRwA",
+  authDomain: "proyectointegral-19e57.firebaseapp.com",
+  projectId: "proyectointegral-19e57",
+  storageBucket: "proyectointegral-19e57.firebasestorage.app",
+  messagingSenderId: "944464810265",
+  appId: "1:944464810265:web:f63c31db8f764e5dbe3588",
+  measurementId: "G-F6Z16PJ2CC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
